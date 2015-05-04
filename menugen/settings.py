d4917568e43d12ca4e508492abb146428d17f97d
km <menager.kiyoaki@gmail.com>
2015-05-04 02:40:29 +0200

    templates: tastes, makes aliments list dynamic

--- menugen/settings.py
+++ menugen/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf import global_settings
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -95,6 +96,10 @@ MEDIA_URL = '/media/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
 )
 
 LOGIN_URL = "/sign-in"
