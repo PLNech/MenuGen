5940adb44ab33572e1093a20f34c0f3305be1c6e
km <menager.kiyoaki@gmail.com>
2015-05-06 21:12:52 +0200

    templates: makes planning width identical

--- menus/templatetags/planning_extras.py
+++ menus/templatetags/planning_extras.py
@@ -8,3 +8,8 @@ DAY_SLOT = ['Matin', 'Collation', 'Déjeuner', 'Collation', 'Dîner']
 @register.filter(name='meal_slot_name')
 def meal_slot_name(value):
     return DAY_SLOT[int(value)]
+
+
+@register.filter(name='column_size')
+def column_size(value):
+    return int(round(12 / value, 0))
