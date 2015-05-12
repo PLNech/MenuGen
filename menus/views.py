3833fb717e173c11923aa2ab49261ed6d76d90b7
Guillaume Hélouis <guillaume.helouis@epita.fr>
2015-05-12 21:02:54 +0200

    Fix dietetics failed import

--- menus/views.py
+++ menus/views.py
@@ -1,4 +1,4 @@
-from dietetics import Calculator
+from menus.algorithms.dietetics import Calculator
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotAllowed
