c4b7d4cc2a2ed2952f6c556ed414016d3f09d986
Guillaume Hélouis <guillaume.helouis@epita.fr>
2015-11-04 22:45:22 +0100

    Tastes: search recipe or ingredient

--- application/static/services.js
+++ application/static/services.js
@@ -4,4 +4,10 @@
 angular.module('MenuGen.services', ['ngResource'])
     .factory('Profile', function($resource) {
         return $resource('/api/profiles/:id/');
+    })
+    .factory('SearchRecipe', function($resource) {
+        return $resource('/api/recipes?name=:name');
+    })
+    .factory('SearchIngredient', function($resource) {
+        return $resource('/api/ingredients?name=:name');
     });
