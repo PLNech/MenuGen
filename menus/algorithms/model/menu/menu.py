e3a14a8ad2aa0828e5af4c0910999f1cadbdc011
Paul-Louis NECH <me@plnech.fr>
2015-11-18 19:19:13 +0100

    Menu: Corrected meal generation (now with fixed gene length)

--- menus/algorithms/model/menu/menu.py
+++ menus/algorithms/model/menu/menu.py
@@ -1,13 +1,14 @@
-import math
-
-__author__ = 'PLNech'
+import logging
+from random import shuffle
 
 from menus.algorithms.dietetics import DieteticsNeeds
-from menus.algorithms.model.menu.menu_manager import MenuManager
-
 from menus.algorithms.model.individual import Individual
 from menus.algorithms.model.menu.dish import Dish
+from menus.algorithms.model.menu.menu_manager import MenuManager
 from menus.algorithms.utils.config import Config
+
+__author__ = 'PLNech'
+logger = logging.getLogger("menus")
 
 
 class Menu(Individual):  # TODO Document!
@@ -18,6 +19,7 @@ class Menu(Individual):  # TODO Document!
             self.genes = genes
         else:
             self.genes = []
+
         self.fitness = 0
         self.fitness_calories = 0
         self.fitness_proteins = 0
@@ -36,6 +38,21 @@ class Menu(Individual):  # TODO Document!
 
     def __repr__(self):
         return "Menu" + str(self)
+
+    def reset_fitness(self):
+        self.fitness = 0
+        self.fitness_calories = 0
+        self.fitness_proteins = 0
+        self.fitness_carbohydrates = 0
+        self.fitness_fats = 0
+
+    def reset_nutrients(self):
+        self.calories = 0
+        self.proteins = 0
+        self.carbohydrates = 0
+        self.fats = 0
+
+        self.genes = []
 
     def get_calories(self):
         if self.fitness_calories == 0:
@@ -137,28 +154,79 @@ class Menu(Individual):  # TODO Document!
         :type dish: Dish
         """
         self.genes[index] = dish
-        self.fitness = 0
-        self.fitness_calories = 0
-        self.fitness_proteins = 0
-        self.fitness_carbohydrates = 0
-        self.fitness_fats = 0
+        self.reset_fitness()
 
     def generate(self):
+        """
+        Generates genes from MenuManager dishes
+        :return: True if the generation was successful
+        :rtype bool
+        """
         manager = MenuManager.get()
+        self.genes = []
 
         accu_calories = 0
         over_size = Config.parameters[Config.KEY_OBJECTIVE_CALORIES] * Config.parameters[Config.KEY_OVERWEIGHT_FACTOR]
         genes_length = Config.parameters[Config.KEY_MAX_DISHES]
-        # random.randrange(0, Config.parameters[Config.KEY_MAX_DISHES])  # TODO: Consider random genome length
+        available_dishes = manager.dishes[:]
+        shuffle(available_dishes)
+        ordered_dishes = sorted(available_dishes, key=lambda x: x.calories, reverse=True)
+
         for _ in range(genes_length):
-            dish = manager.get_random()
-            if accu_calories + dish.calories > over_size:
-                break
-            else:
-                accu_calories += dish.calories
-                self.genes.append(dish)
-                # print("Finished generating a menu of %d calories through %d dishes." %
-                #     (accu_calories, len(self.genes)))
+            did_used_ordered = False  # Did we use ordered list ? (if yes, it is useless to try the next smallest)
+
+            dish = available_dishes.pop()
+            while accu_calories + dish.calories > over_size:
+                logger.debug("Dish too calorific (%d + %d = %d > %d)! Let's try a small one..." %
+                            (accu_calories, dish.calories, accu_calories + dish.calories, over_size))
+                if len(available_dishes) is 0:
+                    logger.error("Not enough dishes but no more available... Generation failed.")
+                    return False
+                if did_used_ordered:
+                    logger.debug("Smallest dish was too big. "
+                                 "Let's remove meal's biggest dish and add two small ones instead...")
+                    # No more small dishes, remove biggest and start again adding dishes!
+
+                    # Reset ordered dishes
+                    did_used_ordered = False
+                    ordered_dishes = sorted(manager.dishes[:], key=lambda x: x.calories, reverse=True)
+
+                    # Get biggest dish in menu
+                    max_cal = 0
+                    max_gene = None
+                    for g in self.genes:
+                        if g.calories > max_cal:
+                            max_cal = g.calories
+                            max_gene = g
+                    # Remove it
+                    self.genes.remove(max_gene)
+                    accu_calories -= max_cal
+
+                    # Add a small one instead
+                    smallest = ordered_dishes.pop()
+                    self.genes.append(smallest)
+                    accu_calories += smallest.calories
+
+                    # Then select a small dish to use as valid dish for this iteration
+                    dish = ordered_dishes.pop()
+                else:
+                    dish = ordered_dishes.pop()
+                    did_used_ordered = True
+                    logger.info("Using smallest dish: %d." % dish.calories)
+
+            # Now we have a valid dish, let's add it
+            accu_calories += dish.calories
+            self.genes.append(dish)
+
+            # Finally don't forget to remove it from the other list too.
+            try:
+                available_dishes.remove(dish)
+                ordered_dishes.remove(dish)
+            except ValueError:
+                pass
+
+        logger.debug("Finished generating a menu of %dx calories through %d dishes." % (accu_calories, len(self.genes)))
+        return True
 
     @staticmethod
     def nutrient_fitness(quantity, objective):
