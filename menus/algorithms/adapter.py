f01f3ef6f582574e150d5dd50b299b25aa3bab03
Paul-Louis NECH <me@plnech.fr>
2015-11-18 19:22:18 +0100

    adapter: Refactored for performance, used logging instead of prints

--- menus/algorithms/adapter.py
+++ menus/algorithms/adapter.py
@@ -1,10 +1,35 @@
-import unicodedata
-from menus.algorithms.utils.printer import Printer
-from menus.models import Recipe, IngredientNutriment, Nutriment, RecipeToIngredient
-from .model.menu.dish import Dish
+import logging
+
 import unidecode
 
+from menus.algorithms.model.menu.dish import Dish
+from menus.models import Recipe, IngredientNutriment, Nutriment, RecipeToIngredient
+
 __author__ = 'PLNech'
+logger = logging.getLogger('menus')
+
+recipe2ingredients_objects = RecipeToIngredient.objects
+ingredient_nutriment_objects = IngredientNutriment.objects
+nutriment_objects = Nutriment.objects
+
+unit_coeff_dict = {
+    'mg': 1 / 100000,
+    'cg': 1 / 10000,
+    'dg': 1 / 1000,
+    'g': 1 / 100,
+    'kg': 10,
+    'ml': 1 / 100,
+    'cl': 1 / 10,
+    'l': 10,
+    'cuillere a cafe': 1 / 20,
+    'cuillere a soupe': 1 / 5.55,  # 18g en moyenne
+    'verre': 1,  # 80g farine -> 120g sucre
+    'gousse': 1 / 2.38,  # TODO: Could use ingredient ? Ail 7-12 -> oignon 100g...
+    'tranche': 1 / 1.56,  # 23g pain blanc -> 140 minibaguette
+    'boite': 1,
+    'pot': 1.25,
+    None: 1 / 5,  # No information -> 100g?  # TODO: Refine this by ingredient
+}
 
 
 def dish2recipe(dish):
@@ -16,7 +41,7 @@ def dish2recipe(dish):
     return recipe
 
 
-def recipe2dish(recipe, verbose=1):
+def recipe2dish(recipe):
     """
     :type recipe Recipe
     :return: Dish
@@ -24,65 +49,39 @@ def recipe2dish(recipe, verbose=1):
 
     dish = Dish(recipe.name, recipe.id)
 
-    if verbose > 0:
-        print("\n------------\nHandling recipe: %s." % dish.name)
-    nuts = IngredientNutriment.objects
+    logger.debug("Handling recipe: %s." % dish.name)
     for ing in recipe.ingredients.all():
-        association = RecipeToIngredient.objects.filter(recipe=recipe, ingredient=ing)[0]
-        if verbose > 1:
-            print("%s %s of %s." % (association.quantity, association.unit, ing.name))
+        association = recipe2ingredients_objects.filter(recipe=recipe, ingredient=ing)[0]
+        logger.debug("%s %s of %s.", association.quantity, association.unit, ing.name)
 
-        ing_cal = calculate_nutrients(association, nuts, "Energie, R\u00E8glement UE N\u00B0 1169/2011", 'kcal')
-        ing_prot = calculate_nutrients(association, nuts, "Prot\u00E9ines")
-        ing_fat = calculate_nutrients(association, nuts, "Lipides")
-        ing_carb = calculate_nutrients(association, nuts, "Glucides")
-        if verbose > 1:
-            print("Ingredient %s: %f cal, %f prot, %f fat, %f carb." %
-                  (ing.name, ing_cal, ing_prot, ing_fat, ing_carb))
-        ingredient_dish = Dish(name=ing.name,
-                               calories=ing_cal, proteins=ing_prot, fats=ing_fat, carbohydrates=ing_carb)
-        dish += ingredient_dish
-    if verbose > 0:
-        print("Nutrients: %f cal, %f prot, %f fat, %f carb.\n------------\n" %
-              (dish.calories, dish.proteins, dish.fats, dish.carbohydrates))
+        ing_cal = calculate_nutrients(association, "Energie, R\u00E8glement UE N\u00B0 1169/2011", 'kcal')
+        ing_prot = calculate_nutrients(association, "Prot\u00E9ines")
+        ing_fat = calculate_nutrients(association, "Lipides")
+        ing_carb = calculate_nutrients(association, "Glucides")
+        logger.debug("Ingredient %s: %.3f cal, %.3f prot, %.3f fat, %.3f carb.",
+                     ing.name, ing_cal, ing_prot, ing_fat, ing_carb)
+        dish += Dish(name=ing.name, calories=ing_cal, proteins=ing_prot, fats=ing_fat, carbohydrates=ing_carb)
+    logger.debug("Nutrients: %.3f cal, %.3f prot, %.3f fat, %.3f carb.\n------------", dish.calories, dish.proteins,
+                dish.fats, dish.carbohydrates)
     return dish
 
 
-def calculate_nutrients(association, nuts, name, unit=None):
+def calculate_nutrients(association, name, unit=None):
     if association.quantity is None or association.quantity is 0:
         association.quantity = 1
 
-    coeff_dict = {
-        'mg': 1 / 100000,
-        'cg': 1 / 10000,
-        'dg': 1 / 1000,
-        'g': 1 / 100,
-        'kg': 10,
-        'ml': 1 / 100,
-        'cl': 1 / 10,
-        'l': 10,
-        'cuillere a cafe': 1 / 20,
-        'cuillere a soupe': 1 / 5.55,  # 18g en moyenne
-        'verre': 1,  # 80g farine -> 120g sucre
-        'gousse': 1 / 2.38,  # TODO: Could use ingredient ? Ail 7-12 -> oignon 100g...
-        'tranche': 1 / 1.56,  # 23g pain blanc -> 140 minibaguette
-        'boite': 1,
-        'pot': 1.25,
-        None: 1 / 5,  # No information -> 100g?  # TODO: Refine this by ingredient
-    }
-
     if unit is not None:
-        nutriment = Nutriment.objects.get(name=name, unit_per_100g=unit)
+        nutriment = nutriment_objects.get(name=name, unit_per_100g=unit)
     else:
-        nutriment = Nutriment.objects.get(name=name)
+        nutriment = nutriment_objects.get(name=name)
 
     ascii_unit = None if association.unit is None else unidecode.unidecode(association.unit)
 
-    link = nuts.get(ingredient=association.ingredient, nutriment=nutriment)
+    link = ingredient_nutriment_objects.get(ingredient=association.ingredient, nutriment=nutriment)
 
-    if ascii_unit in coeff_dict:
-        coeff = coeff_dict[ascii_unit]
+    if ascii_unit in unit_coeff_dict:
+        coeff = unit_coeff_dict[ascii_unit]
     else:
-        print(Printer.err("The following unit could not be quantified: %s." % ascii_unit))
+        logger.error("The following unit could not be quantified: {}.", ascii_unit)
         coeff = 1
     return coeff * link.quantity * association.quantity / association.recipe.amount
