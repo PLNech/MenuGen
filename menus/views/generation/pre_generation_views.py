3e9be4eee0c5f4ab544184c7960ee98134349197
Paul-Louis NECH <me@plnech.fr>
2015-11-30 13:51:58 +0100

    PreGen: Own profile checked by default

--- menus/views/generation/pre_generation_views.py
+++ menus/views/generation/pre_generation_views.py
@@ -33,7 +33,8 @@ def generate_select_profile(request):
     guests = list(guests)
     logger.info("GuestList: %r" % guests)
     guests.append(user.account.profile)
-    return render(request, 'menus/generate/select_profile.html', {'profiles': guests})
+    return render(request, 'menus/generate/select_profile.html', {'profiles': guests,
+                                                                  'user_profile': user.account.profile})
 
 
 def generate_placements_detail(request):
