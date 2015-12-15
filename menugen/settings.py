31dd84405ba773985d54ec79a4a32086378c6436
Paul-Louis NECH <me@plnech.fr>
2015-12-15 17:58:26 +0100

    Revert "Project: Cleaned out rest_framework"
    This reverts commit dca060aa31908218e8464a865ef838342f4114aa.

--- menugen/settings.py
+++ menugen/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
+    'rest_framework',
     'application',
     'stdimage',
     'menus',
--- menus/views/views.py
+++ menus/views/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-# from menus.serializers import *
-# from django.contrib.auth.models import User
+from rest_framework import viewsets
+from menus.serializers import *
+from django.contrib.auth.models import User
 
 
 def landing(request):
@@ -67,72 +68,72 @@ def account(request):
 def call_to_action(request):
     return render(request, 'call_to_action_modal.html', {})
 
-#
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-#
-# class AccountViewSet(viewsets.ModelViewSet):
-#     queryset = Account.objects.all()
-#     serializer_class = AccountSerializer
-#
-#
-# class ProfileViewSet(viewsets.ModelViewSet):
-#     queryset = Profile.objects.all()
-#     serializer_class = ProfileSerializer
-#
-#
-# class RecipeToIngredientViewSet(viewsets.ModelViewSet):
-#     queryset = RecipeToIngredient.objects.all()
-#     serializer_class = RecipeToIngredientSerializer
-#
-#
-# class RecipeViewSet(viewsets.ModelViewSet):
-#     # enable filter with query parameters such as /api/recipes?name=tarte
-#     def get_queryset(self):
-#         name = self.request.query_params.get('name', None)
-#         if name:
-#             return Recipe.objects.filter(name__icontains=name)
-#         return Recipe.objects.all()
-#     serializer_class = RecipeSerializer
-#
-#
-# class DietViewSet(viewsets.ModelViewSet):
-#     queryset = Diet.objects.all()
-#     serializer_class = DietSerializer
-#
-#
-# class IngredientViewSet(viewsets.ModelViewSet):
-#     # enable filter with query parameters such as /api/ingredients?name=sucre
-#     def get_queryset(self):
-#         name = self.request.query_params.get('name', None)
-#         if name:
-#             return Ingredient.objects.filter(name__icontains=name)
-#         return Ingredient.objects.all()
-#     serializer_class = IngredientSerializer
-#
-#
-# class IngredientNutrimentViewSet(viewsets.ModelViewSet):
-#     queryset = IngredientNutriment.objects.all()
-#     serializer_class = IngredientNutrimentSerializer
-#
-#
-# class IngredientFamilyViewSet(viewsets.ModelViewSet):
-#     queryset = IngredientFamily.objects.all()
-#     serializer_class = IngredientFamilySerializer
-#
-#
-# class NutrimentViewSet(viewsets.ModelViewSet):
-#     queryset = Nutriment.objects.all()
-#     serializer_class = NutrimentSerializer
-#
-#
-# class MenuViewSet(viewsets.ModelViewSet):
-#     queryset = Menu.objects.all()
-#     serializer_class = MenuSerializer
-#
-#
-# class MealViewSet(viewsets.ModelViewSet):
-#     queryset = Meal.objects.all()
-#     serializer_class = MealSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class RecipeToIngredientViewSet(viewsets.ModelViewSet):
+    queryset = RecipeToIngredient.objects.all()
+    serializer_class = RecipeToIngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    # enable filter with query parameters such as /api/recipes?name=tarte
+    def get_queryset(self):
+        name = self.request.query_params.get('name', None)
+        if name:
+            return Recipe.objects.filter(name__icontains=name)
+        return Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class DietViewSet(viewsets.ModelViewSet):
+    queryset = Diet.objects.all()
+    serializer_class = DietSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    # enable filter with query parameters such as /api/ingredients?name=sucre
+    def get_queryset(self):
+        name = self.request.query_params.get('name', None)
+        if name:
+            return Ingredient.objects.filter(name__icontains=name)
+        return Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class IngredientNutrimentViewSet(viewsets.ModelViewSet):
+    queryset = IngredientNutriment.objects.all()
+    serializer_class = IngredientNutrimentSerializer
+
+
+class IngredientFamilyViewSet(viewsets.ModelViewSet):
+    queryset = IngredientFamily.objects.all()
+    serializer_class = IngredientFamilySerializer
+
+
+class NutrimentViewSet(viewsets.ModelViewSet):
+    queryset = Nutriment.objects.all()
+    serializer_class = NutrimentSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
