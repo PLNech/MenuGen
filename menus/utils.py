07e7e2af6d6cec906e2abb288f82a1b5fbd1c9ea
Paul-Louis NECH <me@plnech.fr>
2015-12-15 22:58:54 +0100

    list_pk: keys are now unique and listed by numerical order

--- menus/utils.py
+++ menus/utils.py
@@ -10,11 +10,14 @@ def _json_object_hook(d):
 def json2obj(data):
     return json.loads(data, object_hook=_json_object_hook)
 
+
 def list_pk(queryset):
     if queryset is None:
         return "0"
     items = queryset.all()
     if len(items):
-        return ",".join([str(i.pk) for i in sorted(items, key=lambda i: i.pk)])
+        pklist = [str(i.pk) for i in items]
+        uniquelist = sorted(list(set(pklist)), key=lambda i: int(i))
+        return ",".join(uniquelist)
     else:
         return "None"
