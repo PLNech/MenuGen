861f890ea8e1cd4ce2069304c3a3a5f686af1137
Paul-Louis NECH <me@plnech.fr>
2015-12-16 12:49:50 +0100

    FB Share: Pictures on share_recipe

--- menus/static/facebook.js
+++ menus/static/facebook.js
@@ -12,18 +12,18 @@ function share_meal(starter, main, desert, picture_url) {
         function (response) {
         });
 }
-
-function share_recipe(name, picture) {
-    console.log("Sharing recipe: ", name, picture);
+function share_recipe(name, picture_url) {
+    console.log("Sharing recipe: ", name, picture_url);
     var caption = 'MenuGen me conseille cette recette : ' + name + '.';
     var description = 'Et vous, que va-t\'il vous proposer ?';
+    var default_url = 'https://plnech.fr/files/menugen_logo.png';
     FB.ui({
-        method: 'feed',
-        name: 'Mon plat',
-        description: description,
-        picture: 'https://plnech.fr/files/menugen_logo.png',
-        caption: caption
-    },
+            method: 'feed',
+            name: 'Mon plat',
+            description: description,
+            picture: picture_url && picture_url != '' ? picture_url : default_url,
+            caption: caption
+        },
         function (response) {
         });
 }
